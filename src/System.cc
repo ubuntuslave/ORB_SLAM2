6d4bd07b1321e1ae4fd887788dee7ffbc2301196
Carlos Jaramillo <cjaramillo@gc.cuny.edu>
2018-03-14 20:54:21 -0400

    Loading binary vocabulary

--- src/System.cc
+++ src/System.cc
@@ -25,8 +25,13 @@
 #include <thread>
 #include <pangolin/pangolin.h>
 #include <iomanip>
-
 #include <unistd.h>
+
+// Useful for determining the extension of a file name (e.g. an DBoW vocabulary)
+bool has_suffix(const std::string &str, const std::string &suffix) {
+  std::size_t index = str.find(suffix, str.size() - suffix.size());
+  return (index != std::string::npos);
+}
 
 namespace ORB_SLAM2
 {
@@ -59,12 +64,21 @@ System::System(const string &strVocFile, const string &strSettingsFile, const eS
        exit(-1);
     }
 
-
     //Load ORB Vocabulary
-    cout << endl << "Loading ORB Vocabulary. This could take a while..." << endl;
+    cout << endl << "Loading ORB Vocabulary." << endl;
 
     mpVocabulary = new ORBVocabulary();
-    bool bVocLoad = mpVocabulary->loadFromTextFile(strVocFile);
+    bool bVocLoad = false; // chose loading method based on file extension
+    if (has_suffix(strVocFile, ".txt"))
+      {
+          cout << "This could take a while..." << endl;
+          bVocLoad = mpVocabulary->loadFromTextFile(strVocFile);
+      }
+    else
+      {
+          bVocLoad = mpVocabulary->loadFromBinaryFile(strVocFile);
+      }
+
     if(!bVocLoad)
     {
         cerr << "Wrong path to vocabulary. " << endl;
